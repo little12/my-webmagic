5f8dd1f13a703f91c443e1364ea3100cff9b1e2d
little12 <447253227@qq.com>
2019-12-30 15:58:48 +0800

    完成

---
+++ README.md
@@ -0,0 +1,14 @@
+[参考1]github项目   
+[参考2](https://blog.csdn.net/panchang199266/article/details/85413746)  
+webmagic框架运用  
+[参考3 ](https://blog.csdn.net/zbj18314469395/article/details/100642086)  
+selenium自动点击下一页  
+[参考4](https://blog.csdn.net/Eastmount/article/details/80088948)  
+selenium爬取局部动态刷新网站（URL始终固定）  
+[工具]    
+下载对应版本的Chrome driver
+Input   
+需要抓取的网址 页数  
+Output  
+输出到文件所抓取的内容  
+[注意]直接引入webmagic-core依赖会有点bug 因此需要单独引入webmagic-core并安装
--- src/main/java/crawl/JobProcessor.java
+++ src/main/java/crawl/JobProcessor.java
@@ -1,40 +1,96 @@
 package crawl;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.downloader.selenium.SeleniumDownloader;
 import us.codecraft.webmagic.pipeline.FilePipeline;
 import us.codecraft.webmagic.processor.PageProcessor;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
 public class JobProcessor implements PageProcessor {
+    private int pageTotal = 1;
+    private static String url = "http://bcc.blcu.edu.cn/zh/search/35/%E6%89%94%E5%9C%A8";
+    private String word = "扔在";
     //解析页面
     public void process(Page page) {
-        //解析返回的数据page，并且把解析的结果放到ResultItems中
-        // getHtml获取目标页面 div标签 class为mt 下面的h2标签的所有元素
-        page.putField("div", page.getHtml().css("ul.fr a").all());
+        //selenium自动控制
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();//窗口最大化
+        driver.manage().timeouts().implicitlyWait(8, TimeUnit.SECONDS);
+        driver.get(url);
 
-        //Xpath
-        page.putField("div2",page.getHtml().xpath("//div[@id=shortcut]/div/ul/li/a"));
+        //添加句柄
+        String currentWindow = driver.getWindowHandle();
+        Set<String> handles = driver.getWindowHandles();
+        Iterator<String> it = handles.iterator();
+        while (it.hasNext()) {
+            String handle = it.next();
+            if (currentWindow.equals(handle))
+                continue;
+            driver.switchTo().window(handle);
+        }
 
-        //正则表达式
-        page.putField("div3",page.getHtml().css("div.w ul.fr a").regex(".*好.*").all());
+        //获取到元素列表
+        List<WebElement> list = driver.findElements(By.xpath("//td[contains(text(),'CJba')]"));
+        List<String> listStr=new ArrayList<String>();
+        for(WebElement element:list){
+            listStr.add(element.getText());
+            //System.out.println(element.getText());
+        }
 
-        //获取链接
-        page.addTargetRequests(page.getHtml().css("div#shortcut").links().all());
-        page.putField("url", page.getHtml().css("url.fr a").all());
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //从第二页开始迭代
+        for(int i=1;i<pageTotal;i++){
+            //选择下一页元素进行点击
+            driver.findElement(By.cssSelector("div.btn-container ul.pager button:nth-of-type(3)")).click();
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            //获取到元素列表
+            List<WebElement> list1 = driver.findElements(By.xpath("//td[contains(text(),'CJba')]"));
+            // List<String> listStr1=new ArrayList<String>();
+            for(WebElement element:list1){
+                listStr.add(element.getText());
+                //System.out.println(element.getText());
+            }
+        }
+        page.putField(word,listStr);
+       // page.putField("div3",page.getHtml().css("div.container-fluid table tbody td.col-md-5").regex(".*CJba.*").all());
     }
 
-    private Site site=Site.me();
+    private Site site = Site.me()
+            .setSleepTime(1000)
+            .setCycleRetryTimes(3);
     public Site getSite() {
         return site;
     }
 
+
     //主函数 执行爬虫
     public static void main(String[] args){
+        System.setProperty("selenuim_config", "C:\\Users\\LISHUANG\\Downloads\\webmagic-crawl\\src\\main\\resources\\selenmium.properties");
         Spider.create(new JobProcessor())
-                .addUrl("https://www.jd.com/moreSubject.aspx") //设置爬虫的页面
+                .addUrl(url) //设置爬虫的页面
                 .addPipeline(new FilePipeline("C:\\Users\\LISHUANG\\Desktop\\result"))//输出到文件
                 .thread(5)//多线程
+                .setDownloader(new SeleniumDownloader("E:\\chromedriver.exe").setSleepTime(3000))
                 .run();
     }
 }
